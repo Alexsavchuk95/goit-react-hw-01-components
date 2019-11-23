b60dea012454f46cdf419fc9fe00478849e9ca4d
Alexsavchuk95 <savchukalex57@gmail.com>
2019-11-23 23:14:57 +0300

    Updates

---
+++ precache-manifest.bb3503ba322e1b6e27123b675c80dc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfa58926ee11516435bd0ede7895d4b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "4c9ebd05abafd3157df0",
+    "url": "/goit-react-hw-01-components/static/css/main.50ea3240.chunk.css"
+  },
+  {
+    "revision": "c5c04ea3ce20d317fca3",
+    "url": "/goit-react-hw-01-components/static/js/2.3a632355.chunk.js"
+  },
+  {
+    "revision": "4c9ebd05abafd3157df0",
+    "url": "/goit-react-hw-01-components/static/js/main.90175d42.chunk.js"
+  },
+  {
+    "revision": "4d9ce36fabe79d32c0ed",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.7a5c0bc9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Alexsavchuk95/goit-react-hw-01-components/precache-manifest.5b7ad979efd51064bd95de080e0101e4.js"
+  "/goit-react-hw-01-components/precache-manifest.bb3503ba322e1b6e27123b675c80dc21.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Alexsavchuk95/goit-react-hw-01-components/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
